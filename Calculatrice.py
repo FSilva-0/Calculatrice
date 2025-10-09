91570fbfbceb9e37367eaf22cde8bb4a509f61de
Fran <franciscosilvapizarro@gmail.com>
2025-10-09 12:17:05 +0200

    class mére

---
+++ Calculatrice.py
@@ -0,0 +1,24 @@
+class Calculatrice():
+    def addition(self,a,b):
+        return
+
+    def subtraction(self,a,b):
+        return
+
+    def multiplication(self,a,b):
+        return
+
+    def quotient(self,a,b):
+        if b == 0:
+            print("On ne peut diviser par 0 gros con")
+
+        return
+
+    def puissance(self,a,b):
+        return
+
+    def fibonacci(self,n):
+        return
+
+    def prime(self,p):
+        return
